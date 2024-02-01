1533f97c719ea8695537807347db560489e42837
Tatiana Ivanova <tati.ivanova.87@gmail.com>
2024-02-02 02:37:06 +0300

    fix: fix url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { App } from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/recipes-project'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
